7f4858cc79165131a306fd41b749153a2077e5b4
ujvsprakash98@gmail.com <ujvsprakash98@gmail.com>
2020-11-12 10:57:20 +0530

    Third Cimmit

---
+++ app/src/main/java/com/example/lntappb2/database/DbAccessObj.java
@@ -0,0 +1,13 @@
+package com.example.lntappb2.database;
+
+public class DbAccessObj {
+
+    public DbAccessObj(){}
+
+    private void openDb(){}
+    private void closeDb(){}
+    public void createRow(String title,String subtitle){}
+    private  void readRow(){}
+    private void updateRow(){}
+    private void deleteRow(){}
+}
